12560721cbc325247348448401b8ae743394b395
Ruth Newman <ruthnewman@Ruths-MacBook-Air.local>
2019-04-26 01:17:41 +0100

    with initialise function, scoreboard and play again button and event listener

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const gameDiv = document.getElementById('game');
+const scoreBoardDiv = document.getElementById('scoreBoard');
+const playAgainButton = document.querySelector('.playAgain');
+const headingTitle = document.querySelector('.heading');
 
 // 1 = wall, 2 = pacman, 3 = ground, 4 = coin
 
-let gameMap = [
-  [1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,4,4,4,4,4,1,4,4,4,4,4,1],
-  [1,4,1,1,1,4,1,4,1,1,1,4,1],
-  [1,4,1,4,4,4,4,4,4,4,1,4,1],
-  [1,4,4,4,1,1,2,1,1,4,4,4,1],
-  [1,4,1,4,4,4,4,4,4,4,1,4,1],
-  [1,4,1,1,4,4,1,4,4,1,1,4,1],
-  [1,4,4,4,4,4,1,4,4,4,4,4,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1],
-];
+let gameMap = [];
+let pacman = {};
+let score;
 
-let pacman = {
-  x: 6,
-  y: 4
+function initialise() {
+  gameMap = [
+    [1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [1,4,4,4,4,4,1,4,4,4,4,4,1],
+    [1,4,1,1,1,4,1,4,1,1,1,4,1],
+    [1,4,1,4,4,4,4,4,4,4,1,4,1],
+    [1,4,4,4,1,1,2,1,1,4,4,4,1],
+    [1,4,1,4,4,4,4,4,4,4,1,4,1],
+    [1,4,1,1,4,4,1,4,4,1,1,4,1],
+    [1,4,4,4,4,4,1,4,4,4,4,4,1],
+    [1,1,1,1,1,1,1,1,1,1,1,1,1],
+  ];
+  pacman = {
+    x: 6,
+    y: 4
+  }
+  score = 0;
+  drawGame();
 }
 
 function drawGame() {
@@ -44,30 +54,83 @@ function drawGame() {
 document.onkeydown = function(event) {
   switch (event.keyCode) {
     case 37:
-      gameMap[pacman.y][pacman.x] = 3;
-      pacman.x = pacman.x - 1;
-      gameMap[pacman.y][pacman.x] = 2;
-      drawGame();
+      if(gameMap[pacman.y][pacman.x-1] !== 1) {
+        if(gameMap[pacman.y][pacman.x-1] === 4) {
+          score += 10;
+          drawScoreBoard(score);
+        }
+        gameMap[pacman.y][pacman.x] = 3;
+        pacman.x = pacman.x - 1;
+        gameMap[pacman.y][pacman.x] = 2;
+        drawGame();
+        if(score === 540) {
+          won();
+        }
+      }
       break;
     case 38:
-      gameMap[pacman.y][pacman.x] = 3;
-      pacman.y = pacman.y - 1;
-      gameMap[pacman.y][pacman.x] = 2;
-      drawGame();
+      if(gameMap[pacman.y-1][pacman.x] !== 1) {
+        if(gameMap[pacman.y-1][pacman.x] === 4) {
+          score += 10;
+          drawScoreBoard(score);
+        }
+        gameMap[pacman.y][pacman.x] = 3;
+        pacman.y = pacman.y - 1;
+        gameMap[pacman.y][pacman.x] = 2;
+        drawGame();
+        if(score === 540) {
+          won();
+        }
+      }
       break;
     case 39:
-      gameMap[pacman.y][pacman.x] = 3;
-      pacman.x = pacman.x + 1;
-      gameMap[pacman.y][pacman.x] = 2;
-      drawGame();
+      if(gameMap[pacman.y][pacman.x+1] !== 1) {
+        if(gameMap[pacman.y][pacman.x+1] === 4) {
+          score += 10;
+          drawScoreBoard(score);
+        }
+        gameMap[pacman.y][pacman.x] = 3;
+        pacman.x = pacman.x + 1;
+        gameMap[pacman.y][pacman.x] = 2;
+        drawGame();
+        if(score === 540) {
+          won();
+        }
+      }
       break;
     case 40:
-      gameMap[pacman.y][pacman.x] = 3;
-      pacman.y = pacman.y + 1;
-      gameMap[pacman.y][pacman.x] = 2;
-      drawGame();
+      if(gameMap[pacman.y+1][pacman.x] !== 1) {
+        if(gameMap[pacman.y+1][pacman.x] === 4) {
+          score += 10;
+          drawScoreBoard(score);
+        }
+        gameMap[pacman.y][pacman.x] = 3;
+        pacman.y = pacman.y + 1;
+        gameMap[pacman.y][pacman.x] = 2;
+        drawGame();
+        if(score === 540) {
+          won();
+        }
+      }
       break;
   }
 }
 
-drawGame();
+function drawScoreBoard(score) {
+  scoreBoardDiv.textContent = score;
+}
+
+function won() {
+    gameDiv.innerHTML = '<h1 class="winningMessage">You Win!</h1>'
+    headingTitle.style.display = 'none';
+    playAgainButton.style.display = 'block';
+    playAgainButton.textContent = 'Play again?';
+}
+
+initialise();
+
+playAgainButton.addEventListener('click', event => {
+  headingTitle.style.display = 'block';
+  playAgainButton.style.display = 'none';
+  initialise();
+})
